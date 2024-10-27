72929fbb43b5ae37ccf2680bfa612e1609bdc1b9
Foucsi <julien.foucartreact@gmail.com>
2024-10-27 21:21:28 +0100

    add dotenv

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+require('dotenv').config();
+require('./infrastructure/database/mongoConnection');
+
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
