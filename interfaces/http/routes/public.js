0483ca24192ec262c956b5da41624c8d5f6d4698
Foucsi <julien.foucartreact@gmail.com>
2024-10-28 18:47:28 +0100

    update comment

--- interfaces/http/routes/public.js
+++ interfaces/http/routes/public.js
@@ -4,14 +4,8 @@ const router = express.Router();
 const UserController = require('../../controllers/UserControllers');
 
 
-// Exemple de route publique : inscription d'un utilisateur
+// Exemple de route publique : inscription d'un utilisateur et connection
 router.post('/register',  UserController.createUser);
 router.post('/login', UserController.loginUser)
-
-
-// Autre exemple de route publique : page d'accueil
-router.get('/', (req, res) => {
-  res.json({ message: 'Welcome to the public API routes!' });
-});
 
 module.exports = router;
