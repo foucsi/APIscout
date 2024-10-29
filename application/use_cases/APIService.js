daaac62b275defd1d34b0d66986f971924bef00a
Foucsi <julien.foucartreact@gmail.com>
2024-10-29 12:42:17 +0100

    create create API

--- application/use_cases/APIService.js
+++ application/use_cases/APIService.js
@@ -1,1 +1,18 @@
 const APIModel = require('../../infrastructure/database/models/APIModel')
+
+
+// Objectif : contient la ligique metier de création d'une api par un utilisateur
+
+const createApi = async({name, description, version, baseUrl}) =>{
+    const api = new APIModel({
+        name,
+        description,
+        version,
+        baseUrl
+    })
+
+    await api.save()
+    return api
+}
+
+module.exports = createApi
