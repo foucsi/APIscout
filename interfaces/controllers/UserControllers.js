bd4fed6fbd1263000ee6f9684b50edef4f213d50
Foucsi <julien.foucartreact@gmail.com>
2024-10-30 09:30:46 +0100

    update user controllers

--- interfaces/controllers/UserControllers.js
+++ interfaces/controllers/UserControllers.js
@@ -4,6 +4,7 @@ const UserService = require('../../application/use_cases/UserService');
 exports.getAllUsers = async(req, res) => {
     try{
       const users = await UserService.find()
+      res.status(200).json(users)
     }catch(error){
       res.status(500).json({message: error.message})
     }
