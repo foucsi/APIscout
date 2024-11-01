6d1864ab3c75e2882473b5c0a55bae0ba2f900ea
Foucsi <julien.foucartreact@gmail.com>
2024-11-01 20:41:17 +0100

    add UserModel.create repository

--- infrastructure/repositories/UserRepository.js
+++ infrastructure/repositories/UserRepository.js
@@ -1,1 +1,10 @@
 const UserModel = require('../database/models/UserModel')
+
+
+const createUser = async (userData) => {
+    return UserModel.create(userData); // Insère un nouveau document utilisateur
+  };
+  
+  module.exports = {
+    createUser,
+  };
