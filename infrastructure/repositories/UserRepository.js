12a7295494524012b0998be529426b0583794c27
Foucsi <julien.foucartreact@gmail.com>
2024-11-01 20:58:37 +0100

    add repo user findAllUsers

--- infrastructure/repositories/UserRepository.js
+++ infrastructure/repositories/UserRepository.js
@@ -4,7 +4,12 @@ const UserModel = require('../database/models/UserModel')
 const createUser = async (userData) => {
     return UserModel.create(userData); // Insère un nouveau document utilisateur
   };
+
+const findAllUsers = async(userData)=>{
+  return UserModel.find(userData) // Récupére tous les Users de la db
+}
   
   module.exports = {
     createUser,
+    findAllUsers
   };
