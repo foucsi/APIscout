70a5f8358a6bcd73f98d63fc3ced0fff648f8d4a
Foucsi <julien.foucartreact@gmail.com>
2024-11-02 08:42:59 +0100

    add APIReposortory

--- infrastructure/repositories/APIRepository.js
+++ infrastructure/repositories/APIRepository.js
@@ -1,2 +1,9 @@
 const APIModel = require('../database/models/APIModel')
 
+const createAPI = async(database)=>{
+    return await APIModel.create(database)
+}
+
+module.exports = {
+    createAPI
+}
