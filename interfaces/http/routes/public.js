69ec093faaf74f70ca462599c57ca77cf0940826
Foucsi <julien.foucartreact@gmail.com>
2024-11-10 16:53:15 +0100

    update comment

--- interfaces/http/routes/public.js
+++ interfaces/http/routes/public.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const UserController = require('../../controllers/UserControllers');
 
 
-// Exemple de route publique : inscription d'un utilisateur et connection
+// Route sans middleware authentification => inscription d'un utilisateur et connection
 router.post('/register',  UserController.createUser);
 router.post('/login', UserController.loginUser)
 
