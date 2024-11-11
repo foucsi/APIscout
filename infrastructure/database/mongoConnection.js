4aa097adc1f85f5ab0757ea7b32fd292f6588a34
Foucsi <julien.foucartreact@gmail.com>
2024-11-11 12:25:28 +0100

    update connection database

--- infrastructure/database/mongoConnection.js
+++ infrastructure/database/mongoConnection.js
@@ -3,10 +3,15 @@ const connectionString = process.env.CONNECTION_STRING;
 
 //connection to the database
 
+const connect = async()=>{
+    await mongoose.connect(connectionString, {connectTimeoutMS: 2000});
+    console.log("Successfully connected to the database: APIscout");
+}
+
 const connectDb = async ()=>{
     try{
-        await mongoose.connect(connectionString, {connectTimeoutMS: 2000});
-        console.log("Successfully connected to the database: APIscout");
+        console.log("Connection progress...")
+        setTimeout(connect, 1000)
     }catch(error){
         console.error("Database connection failed. Retrying in 5 seconds...", error);
         setTimeout(connectDb, 5000);
