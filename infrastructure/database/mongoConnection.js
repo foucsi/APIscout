a8eebdeda97562e14d38a0076a9fb794af0afdbb
Foucsi <julien.foucartreact@gmail.com>
2024-11-27 06:51:11 +0100

    update

--- infrastructure/database/mongoConnection.js
+++ infrastructure/database/mongoConnection.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const connectionString = process.env.CONNECTION_STRING;
 
-//connection mongoDb database
+//connection mongoDb database => good practice :)
 const connectWithRetry = async (retries = 5, delay = 5000) => {
   try {
     console.log("Connecting to database...");
